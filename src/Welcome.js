bf756db13db67b879fa65f99ddc6df6a33abc718
Aslam Doctor <aslam.doctor@gmail.com>
2022-01-29 20:59:22 +0530

    welcome screen done

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+export default function Welcome() {
+	return (
+		<div className="app-box">
+			<div className="welcome">
+				<h1>React Quiz App</h1>
+				<p>Trivia Quiz App using React JS</p>
+				<button>Start quiz</button>
+			</div>
+		</div>
+	);
+}
